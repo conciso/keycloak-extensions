432a66f358d678dc63899b136a44a066cd4ae760
Hassan El Mailoudi <hassan.elmailoudi@conciso.de>
2024-06-13 09:04:40 +0200

    Edit JUnit-test to extract expected successfull login time from access-token instead of logs

--- user-management/src/test/java/de/conciso/keycloak/user/mgmt/ListenerITest.java
+++ user-management/src/test/java/de/conciso/keycloak/user/mgmt/ListenerITest.java
@@ -1,5 +1,8 @@
 package de.conciso.keycloak.user.mgmt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.github.dockerjava.api.exception.UnauthorizedException;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.*;
@@ -8,20 +11,12 @@ import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -36,12 +31,9 @@ public class ListenerITest {
     private static final String ADMIN_PASS = "admin";
     private static String authUrl;
     private static Keycloak keycloakAdminClient;
-    private static String attributeValue;
     private static Instant expectedTimeValue;
     private static Instant actualTimeValue;
-    private static final List<String> loginTimes = new ArrayList<>();
-    private static final Pattern LOG_PATTERN = Pattern.compile("(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}) INFO  .*type=LOGIN.*username=test");
-    private static final DateTimeFormatter LOG_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS");
+    private static String userAccessToken;
 
     @Container
     private static final KeycloakContainer keycloak =
@@ -50,17 +42,7 @@ public class ListenerITest {
             .withEnv("KEYCLOAK_ADMIN", ADMIN_USER)
             .withEnv("KEYCLOAK_ADMIN_PASSWORD", ADMIN_PASS)
             .withEnv("KC_SPI_EVENTS_LISTENER_JBOSS_LOGGING_SUCCESS_LEVEL", "info")
-            .withLogConsumer(new Slf4jLogConsumer(LOGGER) {
-                @Override
-                public void accept(OutputFrame outputFrame) {
-                    String logLine = outputFrame.getUtf8String();
-                    Matcher matcher = LOG_PATTERN.matcher(logLine);
-                    if (matcher.find()) {
-                        loginTimes.add(matcher.group(1));
-                    }
-                    super.accept(outputFrame);
-                }
-            })
+            .withLogConsumer(new Slf4jLogConsumer(LOGGER))
             .withProviderClassesFrom("target/classes")
             .withRealmImportFile("conciso-realm.json");
 
@@ -86,9 +68,10 @@ public class ListenerITest {
 
     @Test
     @Order(3)
-    public void assertLastSuccessfulLoginAtMatchesExpected() {
-        extractLastLoginTimeFromLogs();
-        assertTrue(calculateDifferenceInSecondsBetweenInstants(actualTimeValue, expectedTimeValue) <= 1,
+    public void assertLastSuccessfulLoginAtAttributeMatchesExpected() {
+        extractLastSuccessfullyLoginTimeFromAccessToken();
+        long diff = calculateDifferenceInSecondsBetweenInstants(actualTimeValue, expectedTimeValue);
+        assertTrue(diff <= 1,
             String.format("Expected time <%s> and actual time <%s> differ by more than 1 second.", expectedTimeValue, actualTimeValue));
     }
 
@@ -99,8 +82,9 @@ public class ListenerITest {
         loginWithUserAccount();
         loginWithAdminAccount();
         extractAttributeValue();
-        extractLastLoginTimeFromLogs();
-        assertTrue(calculateDifferenceInSecondsBetweenInstants(actualTimeValue, expectedTimeValue) <= 1,
+        extractLastSuccessfullyLoginTimeFromAccessToken();
+        long diff = calculateDifferenceInSecondsBetweenInstants(actualTimeValue, expectedTimeValue);
+        assertTrue(diff <= 1,
             String.format("Expected time <%s> and actual time <%s> differ by more than 1 second.", expectedTimeValue, actualTimeValue));
     }
 
@@ -113,7 +97,7 @@ public class ListenerITest {
                 .password("test")
                 .clientId("admin-cli")
                 .build();
-            String accessToken = keycloakUserClient.tokenManager().getAccessToken().getToken();
+            userAccessToken = keycloakUserClient.tokenManager().getAccessToken().getToken();
         } catch (Exception e) {
             throw new UnauthorizedException("User Authentication Failed.");
         }
@@ -137,14 +121,17 @@ public class ListenerITest {
     public void extractAttributeValue() {
         UserRepresentation testUser = keycloakAdminClient.realm(REALM_NAME).users().search("test").get(0);
         assertNotNull(testUser.getAttributes());
-        attributeValue = testUser.getAttributes().get("lastSuccessfulLoginAt").get(0);
+        String attributeValue = testUser.getAttributes().get("lastSuccessfulLoginAt").get(0);
         actualTimeValue = Instant.parse(attributeValue);
     }
 
-    public void extractLastLoginTimeFromLogs() {
-        Assertions.assertFalse(loginTimes.isEmpty(), "No LOGIN logs found for user 'test'");
-        String lastLoginTimeStr = loginTimes.get(loginTimes.size() - 1);
-        expectedTimeValue = LocalDateTime.parse(lastLoginTimeStr, LOG_TIME_FORMATTER).toInstant(ZoneOffset.UTC);
+    public void extractLastSuccessfullyLoginTimeFromAccessToken() {
+        try {
+            DecodedJWT decodedJWT = JWT.decode(userAccessToken);
+            expectedTimeValue = decodedJWT.getIssuedAt().toInstant();
+        } catch (JWTDecodeException e) {
+            throw new JWTDecodeException(e.getMessage());
+        }
     }
 
     public long calculateDifferenceInSecondsBetweenInstants(Instant actualTime, Instant expectedTime) {
